e2ebabac377c1ca23f20a2bb0979364146ec0345
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-20 22:55:44 -0400

    added config to send adecuate error mensaje

--- src/main/java/com/sanosysalvos/bff/config/RestClientConfig.java
+++ src/main/java/com/sanosysalvos/bff/config/RestClientConfig.java
@@ -4,19 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.server.ResponseStatusException;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(JwtForwardingInterceptor jwtInterceptor) {
+    public RestClient restClientGlobal(JwtForwardingInterceptor jwtInterceptor) {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(10000);
+        factory.setReadTimeout(15000);
 
         return RestClient.builder()
                 .requestFactory(factory)
                 .requestInterceptor(jwtInterceptor)
+                .defaultStatusHandler(
+                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                        (request, response) -> {
+                            throw new ResponseStatusException(response.getStatusCode(), "Error en el microservicio");
+                        }
+                )
                 .build();
     }
 }
